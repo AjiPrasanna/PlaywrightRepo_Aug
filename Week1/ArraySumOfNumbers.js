fc78a42949a151ce65bb0d676e57fa281aee3de6
AjiPrasanna <ajithprasannad73@gmail.com>
2026-08-12 09:45:38 +0530

    Add files via upload
    Week2

---
+++ Week1/ArraySumOfNumbers.js
@@ -0,0 +1,15 @@
+
+// Create an array with numeric values
+let arr = [10, 20, 1, 6, 7, 22, 11, 10]
+
+// Declare a variable to store the sum
+let sum = 0
+
+// Use a loop to iterate through the array
+for (let i = 0; i<arr.length; i++) {
+    sum = sum+arr[i]
+    
+}
+// Print the final sum
+
+console.log("The array of final sum is...." + sum);
